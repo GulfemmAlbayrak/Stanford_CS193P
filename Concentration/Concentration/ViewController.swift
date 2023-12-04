b3c953fe2f1491278c7eec6ca5d25beddb704eb0
Gülfem Albayrak <gulfemalbayrak@gmail.com>
2023-12-04 14:35:24 +0300

    first lesson finished

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Gülfem Albayrak on 2.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices: Array<String> = ["🎃","👻","🎃","👻"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+
+    func flipCard(withEmoji emoji:String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = .white
+        }
+    }
+
+}
+
